275bcb0934dad08e79fdbb44f375286a9d6ff0e0
GuilhermeGiusti <guilherme.giusti04@gmail.com>
2025-02-09 09:47:30 -0300

    teste

---
+++ src/main/java/com/soustify/App.java
@@ -0,0 +1,13 @@
+package com.soustify;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
